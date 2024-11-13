68f45b500df8f631d92dce63b80388985222878f
Vitalii Sh <wwvitaliy@gmail.com>
2024-11-13 16:52:26 -0400

    SerializationTry

---
+++ src/main/java/module10/theory/SerializationTry.java
@@ -0,0 +1,33 @@
+package module10.theory;
+
+import java.io.*;
+
+class SerializationTry {
+    private static final String MODULE_10_THEORY = "src/main/java/module10/theory/";
+    private static final String PATH_TO = MODULE_10_THEORY + "seriliazationTry.dat";
+    private static final String PATH_FROM = MODULE_10_THEORY + "seriliazationTry.dat";
+
+
+    private record Person(String name, int age, double height, boolean married)
+            implements Serializable {
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH_TO));
+             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH_FROM));
+        ) {
+            oos.writeObject(new Person("Sam", 33, 178, true));
+            oos.writeObject(new Person("Tom", 30, 175, true));
+
+            Person person = (Person) ois.readObject();
+            System.out.println("person = " + person + ", " + person.getClass());
+
+            person = (Person) ois.readObject();
+            System.out.println("person = " + person + ", " + person.getClass());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
